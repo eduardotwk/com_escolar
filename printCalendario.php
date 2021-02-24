c2e1a070f68832d1c99056f8379945aca4c5be9e
Eduardo Delgado <eduardoo.andrees@gmail.com>
2021-02-24 03:50:52 -0300

    calendario imprimir y descargar

--- printCalendario.php
+++ printCalendario.php
@@ -1,4 +1,4 @@
 <?php
 header('Content-type: application/pdf');
-readfile("/usr/share/nginx/beta.compromisoescolar.com/pdf_calendario/calendario.pdf");
+readfile("/usr/share/nginx/beta.compromisoescolar.com/pdf_calendario/Calendario.pdf");
 ?>
