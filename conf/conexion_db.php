8eee6c073a18e30b09034c50d4112c0392c73d98
eduardotwk <54043365+eduardotwk@users.noreply.github.com>
2021-03-04 11:09:32 -0300

    Add files via upload

---
+++ conf/conexion_db.php
@@ -0,0 +1,52 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'carlos');
+define('DB_PASS', 'secret');
+define('DB_NAME', 'compromiso_escolar_corfo');
+define('DB_PORT', '3306');
+
+function connectDB() {
+    return Conexion();
+}
+
+function connectDB_demos() {
+    return Conexion();
+}
+
+function Conexion() {
+    $lista = array(
+        '127.0.0.1',
+        '::1'
+    );
+
+    try {
+        if(in_array($_SERVER['REMOTE_ADDR'], $lista)){
+            $conn = new PDO(
+
+                
+                "mysql:host=localhost; dbname=compromiso_escolar_corfo;charset=UTF8", 
+                "carlos",//"carlos", //root
+                "root"//"secret"
+            );      
+            $conn->setAttribute(
+                PDO :: ATTR_ERRMODE, 
+                PDO :: ERRMODE_EXCEPTION
+            );
+            return $conn;
+        } else {
+            $conn = new PDO(
+                "mysql:host=167.71.191.60; dbname=compromiso_escolar_corfo;charset=UTF8",
+                "root",
+                "92mbx6#p^wq@hac^"
+            ); 
+            $conn->setAttribute(
+                PDO :: ATTR_ERRMODE, 
+                PDO :: ERRMODE_EXCEPTION
+            );
+            return $conn;
+        }
+    } catch (Exception $e) {
+        exit ("Excepción capturada: ".$e->getMessage());
+    }
+}
